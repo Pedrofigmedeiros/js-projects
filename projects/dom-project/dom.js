4c48ad5464ac5e9b282142a03ad829188644f10a
Pedro Medeiros <pedrofigmedeiros@gmail.com>
2025-01-24 09:33:05 -0300

    adding more features and styling the calculator

---
+++ projects/dom-project/dom.js
@@ -0,0 +1,29 @@
+function handleCostKey(event) {
+  if (event.key === 'Enter') {
+    calculateTotal();
+  }
+}
+
+function calculateTotal() {
+  const inputElement = document.querySelector('.js-cost-input');
+  let cost = Number(inputElement.value);
+
+  if (cost < 40) {
+    cost = cost + 10;
+  } 
+
+  document.querySelector('.js-total-cost')
+    .innerHTML = `The total cost is $${cost}.`;
+}
+
+function subscribe() {
+  const buttonElement = document.querySelector('.js-subscribe-button');
+
+  if (buttonElement.innerText === 'Subscribe') {
+    buttonElement.innerHTML = 'Subscribed';
+    buttonElement.classList.add('is-subscribed');
+  } else {
+    buttonElement.innerHTML = 'Subscribe';
+    buttonElement.classList.remove('is-subscribed');
+  }
+}
